63041c1139ad363072a41fa41f6b566b89d95842
Natalia Taira <nataliagtaira@gmail.com>
2023-12-22 02:50:09 -0300

    Conclusão da atividade 2

--- menu.php
+++ menu.php
@@ -1,80 +1,94 @@
+
+<?php
+    $categoriasMenu = array(
+        "Dashboard",
+        "Cadastro" => array(
+            "Cliente",
+            "Fornecedor",
+            "Usuário",
+            "Produtos",
+            "Perfil de acesso"
+        ),
+        "Relatório" => array(
+            "Cliente",
+            "Faturamento",
+            "Produtos"
+        ));
+
+    /**
+     * Essa função ordena as categorias e subcategorias
+     * 
+     * @param array $categoriasMenu array com as categorias e subcategorias
+     * @return $categoriasMenu array com categorias e subcategorias ordenadas em ordem alfabética
+     */
+    function ordemAlfabetica(array $categoriasMenu) : array
+    {
+        ksort($categoriasMenu);
+        foreach ($categoriasMenu as &$categoria) {
+            if (is_array($categoria)) {
+                sort($categoria);
+            }
+        }
+        return $categoriasMenu;
+    }
+?>
+
 <!-- BEGIN SIDEBAR -->
 <div class="page-sidebar-wrapper">
-		<div class="page-sidebar navbar-collapse collapse">
-			<!-- BEGIN SIDEBAR MENU -->
-			<ul class="page-sidebar-menu">
-				<li class="sidebar-toggler-wrapper">
-					<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-					<div class="sidebar-toggler hidden-phone">
-					</div>
-					<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-				</li>
-				<li class="sidebar-search-wrapper">
-					<!-- BEGIN RESPONSIVE QUICK SEARCH FORM -->
-					<form class="sidebar-search" action="extra_search.html" method="POST">
-						<div class="form-container">
-							<div class="input-box">
-								<a href="javascript:;" class="remove"></a>
-								<input type="text" placeholder="Search..."/>
-								<input type="button" class="submit" value=" "/>
-							</div>
-						</div>
-					</form>
-					<!-- END RESPONSIVE QUICK SEARCH FORM -->
-				</li>
-				<li class="start active ">
-					<a href="index.html">
-					<i class="fa fa-home"></i>
-					<span class="title">
-						Dashboard
-					</span>
-					<span class="selected">
-					</span>
-					</a>
-				</li>
-				<!--Cliente-->
-				<li class="">
-					<a href="javascript:;">
-					<i class="fa fa-file-text"></i>
-					<span class="title">
-						Cadastro
-					</span>
-					<span class="arrow ">
-					</span>
-					</a>
-					<ul class="sub-menu">
-						<li>
-							<a href="#">Cliente</a>
-						</li>
-						<li>
-							<a href="#">Fornecedor</a>
-						</li>
-						<li>
-							<a href="#">Usuário</a>
-						</li>
-					</ul>
-				</li>
-				<!--Relatorio-->
-				<li class="">
-					<a href="javascript:;">
-					<i class="fa fa-bar-chart-o"></i>
-					<span class="title">
-						Relatório
-					</span>
-					<span class="arrow ">
-					</span>
-					</a>
-					<ul class="sub-menu">
-						<li>
-							<a href="#">Cliente</a>
-						</li>
-						<li>
-							<a href="#">Faturamento</a>
-						</li>
-					</ul>
-				</li>
-			</ul>
-			<!-- END SIDEBAR MENU -->
-		</div>
-	</div>
-	<!-- END SIDEBAR -->
+    <div class="page-sidebar navbar-collapse collapse">
+        <!-- BEGIN SIDEBAR MENU -->
+        <ul class="page-sidebar-menu">
+            <li class="sidebar-toggler-wrapper">
+                <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+                <div class="sidebar-toggler hidden-phone">
+                </div>
+                <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+            </li>
+            <li class="sidebar-search-wrapper">
+                <!-- BEGIN RESPONSIVE QUICK SEARCH FORM -->
+                <form class="sidebar-search" action="extra_search.html" method="POST">
+                    <div class="form-container">
+                        <div class="input-box">
+                            <a href="javascript:;" class="remove"></a>
+                            <input type="text" placeholder="Search..."/>
+                            <input type="button" class="submit" value=" "/>
+                        </div>
+                    </div>
+                </form>
+                <!-- END RESPONSIVE QUICK SEARCH FORM -->
+            </li>
+            <?php
+                $menuOrdenado = ordemAlfabetica($categoriasMenu);
+                foreach ($menuOrdenado as $chave => $categorias) {
+                    if (is_array($categorias)) {
+                        echo "<li class=''>";
+                        echo "<a href='javascript:;'>";
+                        if ($chave == "Cadastro") {
+                            echo "<i class='fa fa-file-text'></i>";
+                        }
+                        else if ($chave == "Relatório") {
+                            echo "<i class='fa fa-bar-chart-o'></i>";
+                        }
+                        echo "<span class='title'>". $chave ."</spam>";
+                        echo "<span class='arrow '></span>";
+                        echo "</a>";
+                        echo "<ul class='sub-menu'>";
+                        foreach ($categorias as $subcategoria) {
+                            echo '<li><a href="#">' . $subcategoria . '</a></li>';
+                        }
+                        echo '</ul>';
+                        echo '</li>';
+                    } else {
+                        echo '<li>';
+                        echo '<a href="#">';
+                        echo '<i class="fa fa-home"></i>';
+                        echo '<span class="title">'. $categorias .'</span>';
+                        echo '</a>';
+                        echo '</li>';
+                    }
+                }
+            ?>
+        </ul>
+        <!-- END SIDEBAR MENU -->
+    </div>
+</div>
